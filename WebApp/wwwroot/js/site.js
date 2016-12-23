16193a2165b642b6df0b63f732f01c248e1025af
RickShahid <rick.shahid@live.com>
2016-12-23 09:26:42 -0800

    Workflow update

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -6,36 +6,37 @@ function ReplaceAll(text, find, replace) {
     var regExp = new RegExp(find, "g");
     return text.replace(regExp, replace);
 }
-function CreateTip(targetId, tipText, tipPosition) {
-    $("#" + targetId).qtip({
+function CreateTip(targetId, tipText, tipPosition, hideEvent) {
+   $("#" + targetId).qtip({
         content: { text: tipText },
         position: tipPosition,
-        show: { delay: 2000 }
+        show: { delay: 1000 },
+        hide: { event: hideEvent }
     });
 }
-function CreateTipTop(targetId, tipText, adjustX, adjustY) {
+function CreateTipTop(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "bottom center", at: "top center", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
-function CreateTipTopLeft(targetId, tipText, adjustX, adjustY) {
+function CreateTipTopLeft(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "bottom center", at: "top left", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
-function CreateTipBottom(targetId, tipText, adjustX, adjustY) {
+function CreateTipBottom(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "top center", at: "bottom center", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
-function CreateTipBottomLeft(targetId, tipText, adjustX, adjustY) {
+function CreateTipBottomLeft(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "top center", at: "bottom left", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
-function CreateTipLeft(targetId, tipText, adjustX, adjustY) {
+function CreateTipLeft(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "right center", at: "left center", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
-function CreateTipRight(targetId, tipText, adjustX, adjustY) {
+function CreateTipRight(targetId, tipText, adjustX, adjustY, hideEvent) {
     var tipPosition = { my: "left center", at: "right center", adjust: { x: adjustX, y: adjustY } };
-    CreateTip(targetId, tipText, tipPosition);
+    CreateTip(targetId, tipText, tipPosition, hideEvent);
 }
 function SetTipVisible(targetId, tipVisible) {
     $("#" + targetId).qtip("toggle", tipVisible);
