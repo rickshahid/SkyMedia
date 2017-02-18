a4edb85e8d0110b4fb3e9bdd3c0938ceb5287871
RickShahid <rick.shahid@live.com>
2017-02-17 19:48:20 -0800

    Baseline

---
+++ DocumentDB/Metadata/Functions/isTimecodeFragment.js
@@ -0,0 +1,9 @@
+function isTimecodeFragment(timeSeconds, timeScale, fragment) {
+
+    var timeStart = fragment.start / timeScale;
+
+    var timeEnd = (fragment.start + fragment.duration) / timeScale;
+
+    return timeSeconds >= timeStart && timeSeconds <= timeEnd;
+
+}
