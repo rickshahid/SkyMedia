04fa7d8dfc97d58d7231575f98e761b3f36b0ad7
Rick Shahid <ricksha@microsoft.com>
2017-10-18 10:21:27 -0700

    Baseline

--- ResourceTemplates/Deployment.Global.ps1
+++ ResourceTemplates/Deployment.Global.ps1
@@ -1,8 +1,13 @@
 ﻿# (Get-Module Azure -ListAvailable).Version (v4.4.1 - October 2017)
 # Login-AzureRmAccount
 
-# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Insights).ResourceTypes | Where-Object ResourceTypeName -eq components).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Storage).ResourceTypes | Where-Object ResourceTypeName -eq storageAccounts).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.DocumentDB).ResourceTypes | Where-Object ResourceTypeName -eq databaseAccounts).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Search).ResourceTypes | Where-Object ResourceTypeName -eq searchServices).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Cdn).ResourceTypes | Where-Object ResourceTypeName -eq profiles).ApiVersions
 # ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq trafficManagerProfiles).ApiVersions
+# ((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Insights).ResourceTypes | Where-Object ResourceTypeName -eq components).ApiVersions
 
 $appName = "Azure.Sky.Media"
 
@@ -13,10 +18,22 @@ $resourceGroupName = ($appName + "-US.Central")
 $regionLocation = "Central US"
 
 $templateParameters = @{
+	"storageServiceName" = "SkyMedia"
+	"databaseServiceName" = "SkyMedia"
+	"databaseDataRegions" = "South Central US", "West US", "East US"
+	"searchServiceName" = "SkyMedia"
+	"searchServiceRegion" = "South Central US"
+	"searchServiceTier" = "Free"
+	"searchServiceReplicaCount" = 1
+	"searchServicePartitionCount" = 1
+	"functionAppName" = "SkyFunctions-USCentral"
+	"contentDeliveryProfileName" = "SkyMedia-Akamai"
+	"contentDeliverySubdomainName" = "SkyStorage"
+	"trafficManagerSubdomainName" = "SkyMedia"
+	"trafficManagerRoutingMethod" = "Performance"
+	"trafficManagerRoutingTimeToLive" = 30 # seconds
 	"appInsightsName" = "Azure Sky Media"
 	"appInsightsRegion" = "South Central US"
-	"trafficManagerSubdomainName" = "SkyMedia"
-	"trafficManagerTimeToLive" = 30
 }
 
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction Ignore
