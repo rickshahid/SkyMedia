1a014f07d60616b7e92a5e20393e856a72a3142a
Rick Shahid <rick.shahid@live.com>
2020-03-07 15:37:42 -0800

    Add files via upload

--- terraform/examples/vfxt/3-filers/main.tf
+++ terraform/examples/vfxt/3-filers/main.tf
@@ -1,4 +1,4 @@
-// customize the simple VM by editing the following local variables!!
+// customize the simple VM by editing the following local variables
 locals {
     // the region of the deployment
     location = "eastus"
@@ -200,12 +200,12 @@ output "controller_address" {
   value = module.vfxtcontroller.controller_address
 }
 
-output "management_ip" {
-    value = avere_vfxt.vfxt.vfxt_management_ip
-}
-
 output "ssh_command_with_avere_tunnel" {
     value = "ssh -L443:${avere_vfxt.vfxt.vfxt_management_ip}:443 ${module.vfxtcontroller.controller_username}@${module.vfxtcontroller.controller_address}"
+}
+
+output "management_ip" {
+    value = avere_vfxt.vfxt.vfxt_management_ip
 }
 
 output "mount_addresses" {
