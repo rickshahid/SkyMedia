002a0c8c58e5e8f772b7fec7bb853f754eaa9d24
Rick Shahid <rick.shahid@live.com>
2020-03-08 12:13:52 -0700

    Add files via upload

--- terraform/examples/vfxt/3-filers/backend.tf
+++ terraform/examples/vfxt/3-filers/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "mediastudio"
     container_name       = "terraform"
+    key                  = "tfstate"
     use_msi              = true
   }
 }
