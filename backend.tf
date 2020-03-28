bf844197440be1233e4c0f2e68445b7880f6e465
Rick Shahid <rick.shahid@live.com>
2020-03-28 11:32:18 -0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Azure.Media.Studio0-Storage"
-    storage_account_name = "mediastudio"
-    container_name       = "terraform"
-    key                  = "tfstate"
-    use_msi              = true
-  }
-}
