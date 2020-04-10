72b81cc65c671d385178490237ecc56bb688328d
Rick Shahid <rick.shahid@live.com>
2020-04-10 01:43:01 -0700

    Add files via upload

---
+++ getAppToken.py
@@ -0,0 +1,18 @@
+import sys
+import jwt
+import time
+
+appId = sys.argv[0]
+appKey = sys.argv[1]
+appTokenSeconds = sys.argv[2]
+appTokenEncryption = sys.argv[3]
+
+timeEpochSeconds = int(time.time())
+appTokenPayload = {
+  'iss': appId,
+  'iat': timeEpochSeconds,
+  'exp': timeEpochSeconds + appTokenSeconds
+}
+
+appToken = jwt.encode(appTokenPayload, appKey, algorithm=appTokenEncryption)
+print(appToken.decode())
