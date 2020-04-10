b0485c221dd7948597a13b9a82d9df2352b1ecf6
Rick Shahid <rick.shahid@live.com>
2020-04-10 04:50:11 -0700

    Update main.tf

--- terraform/examples/vfxt/vmss/main.tf
+++ terraform/examples/vfxt/vmss/main.tf
@@ -1,5 +1,5 @@
 // customize the simple VM by editing the following local variablesxfff
-locals {
+locals {aaa
     // the region of the deployment
     location = "eastus"
     vm_admin_username = "azureuser"
