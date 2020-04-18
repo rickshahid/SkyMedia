38af1c2f5a1fe701f3e17f8d94879987c7c391d4
Rick Shahid <rick.shahid@live.com>
2020-04-17 18:02:37 -0700

    Update main.tf

--- terraform/examples/vfxt/vmss/main.tf
+++ terraform/examples/vfxt/vmss/main.tf
@@ -1,4 +1,4 @@
-// customize the simple VM by editing the following local variables
+// customize the simple VM by editing the following local variables HPC
 locals {
     // the region of the deployment
     location = "eastus"
