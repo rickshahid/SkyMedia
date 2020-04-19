6efe020e7e52292508cb7a76495ec6752f5a263b
Rick Shahid <rick.shahid@live.com>
2020-04-19 15:30:10 -0700

    Update TerraformEnvironment.tf

--- TerraformEnvironment.tf
+++ TerraformEnvironment.tf
@@ -1,14 +1,14 @@
 variable "ARM_TENANT_ID" {
  description = "Azure Active Directory (AAD) tenant identifier"
- type = "string"
+ type = string
 }
 
 variable "ARM_SUBSCRIPTION_ID" {
  description = "Azure subscription identifier for resource deployment"
- type = "string"
+ type = string
 }
 
 variable "ARM_USE_MSI" {
  description = "Enables Azure Managed Service Identity (MSI) context"
- type = "boolean"
+ type = boolean
 }
