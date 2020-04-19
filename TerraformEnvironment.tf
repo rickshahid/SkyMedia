de08619fd2000505f2c116c48960ad3d4d05ce7e
Rick Shahid <rick.shahid@live.com>
2020-04-19 16:19:24 -0700

    Update TerraformEnvironment.tf

--- TerraformEnvironment.tf
+++ TerraformEnvironment.tf
@@ -1,9 +1,9 @@
 variable "ARM_SUBSCRIPTION_ID" {
- description = "Azure service resources subscription identifier"
- type = string
+    description = "Azure service resources subscription identifier"
+    type = string
 }
 
 variable "ARM_USE_MSI" {
- description = "Enables Azure Managed Service Identity (MSI)"
- default = false
+    description = "Enables Azure Managed Service Identity (MSI)"
+    default = false
 }
