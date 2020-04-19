6e146c70dfd5a6cf419593621c53af67deba3ced
Rick Shahid <rick.shahid@live.com>
2020-04-19 16:44:50 -0700

    Delete TerraformEnvironment.tf

--- TerraformEnvironment.tf
+++
@@ -1,9 +0,0 @@
-variable "subscription_id" {
-    description = "Azure service resources subscription identifier"
-    type = string
-}
-
-variable "use_msi" {
-    description = "Enables Azure Managed Service Identity (MSI)"
-    default = false
-}
