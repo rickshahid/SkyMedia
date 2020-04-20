782430a055fdaf9574c8ef680fb92637653db8c7
Rick Shahid <rick.shahid@live.com>
2020-04-19 19:01:48 -0700

    Update AzureEnvironment.Variables.tf

--- AzureEnvironment.Variables.tf
+++ AzureEnvironment.Variables.tf
@@ -1,14 +1,14 @@
-variable "TF_VAR_ARM_TENANT_ID" {
+variable "TF_VAR_tenant_id" {
     description = "Azure Active Directory (AAD) tenant identifier"
     type = string
 }
 
-variable "TF_VAR_ARM_SUBSCRIPTION_ID" {
+variable "TF_VAR_subscription_id" {
     description = "Azure resources subscription identifier"
     type = string
 }
 
-variable "TF_VAR_ARM_USE_MSI" {
+variable "TF_VAR_use_msi" {
     description = "Azure Managed Service Identity (MSI)"
     default = false
 }
