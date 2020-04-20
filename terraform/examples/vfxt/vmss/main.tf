66612f990344187c412c946512b04c3b129f436c
Rick Shahid <rick.shahid@live.com>
2020-04-19 17:40:13 -0700

    Update main.tf

--- terraform/examples/vfxt/vmss/main.tf
+++ terraform/examples/vfxt/vmss/main.tf
@@ -39,10 +39,12 @@ locals {
     cache_policy = "Clients Bypassing the Cluster"
 }
 
-//provider "azurerm" {
-//    version = "~>2.4.0"
-//    features {}
-//}
+provider "azurerm" {
+    version = "~>2.4.0"
+    features {}
+    subscription_id = "3d07cfbc-17aa-41b4-baa1-488fef85a1d3"
+    use_msi         = true    
+}
 
 // the render network
 module "network" {
