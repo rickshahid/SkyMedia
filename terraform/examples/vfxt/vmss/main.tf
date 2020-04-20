76ad4bd70abd8b6b0e6c25c8484714aaaac9640f
Rick Shahid <rick.shahid@live.com>
2020-04-19 18:48:07 -0700

    Update main.tf

--- terraform/examples/vfxt/vmss/main.tf
+++ terraform/examples/vfxt/vmss/main.tf
@@ -42,8 +42,6 @@ locals {
 provider "azurerm" {
     version = "~>2.4.0"
     features {}
-    subscription_id = "3d07cfbc-17aa-41b4-baa1-488fef85a1d3"
-    use_msi         = true    
 }
 
 // the render network
