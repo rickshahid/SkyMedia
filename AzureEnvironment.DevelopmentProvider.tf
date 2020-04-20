d2343be309f44f932811689bf8733d2435d6a43d
Rick Shahid <rick.shahid@live.com>
2020-04-19 17:41:15 -0700

    Update AzureEnvironment.DevelopmentProvider.tf

--- AzureEnvironment.DevelopmentProvider.tf
+++ AzureEnvironment.DevelopmentProvider.tf
@@ -1,6 +1,4 @@
-provider "azurerm" {
-    version = "~>2.4.0"
-    features {}
-    subscription_id = "3d07cfbc-17aa-41b4-baa1-488fef85a1d3"
-    use_msi         = true
-}
+//provider "azurerm" {
+//    subscription_id = "3d07cfbc-17aa-41b4-baa1-488fef85a1d3"
+//    use_msi         = true
+//}
